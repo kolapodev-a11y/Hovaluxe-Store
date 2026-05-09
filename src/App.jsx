0c11758f9e9eba857cb4755c3e3e821a014682c4
kolapodev-a11y <kolapodev@gmail.com>
2026-05-09 10:55:02 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { StorefrontPage } from './pages/StorefrontPage';
 import { AdminPage } from './pages/AdminPage';
+import { PaymentCallbackPage } from './pages/PaymentCallbackPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<StorefrontPage />} />
       <Route path="/admin" element={<AdminPage />} />
+      <Route path="/payment/callback" element={<PaymentCallbackPage />} />
     </Routes>
   );
 }
